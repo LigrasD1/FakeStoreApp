a9db41de6ef2f33ab8469e3f5ef04a8e841c5c75
paezl <paezligras@gmail.com>
2024-09-06 22:34:01 -0300

    Se agrega zip con imagenes de prueba

--- LoginBasico/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LoginBasico.AssemblyInfo.cs
+++ LoginBasico/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LoginBasico.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginBasico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+88c1a93231501c5e57b758b31560ee1c05b55d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e329438aeec1cee9a543fdb66b29ea1b93d8e816")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginBasico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginBasico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
